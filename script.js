f30ba473d4852cac5350f569d14ed176b0143bdf
Noor Elahi <shamsinoorelahi@gmail.com>
2024-09-13 17:16:26 +0500

    assignment 14 jp

---
+++ script.js
@@ -0,0 +1,198 @@
+//qns 1
+
+// let city = prompt("Please enter the name of your city:");
+
+// if (city === "karachi") {
+//    document.write(("Welcome to the city of lights")); 
+// } else {
+//    document.write((`Welcome to ${city}`)); 
+// }
+
+//qns 2
+
+// let gendar = prompt("Please enter your gendar")
+
+// gendar = gendar.trim().toLowerCase();;
+
+// if(gendar === "male") {
+//     document.write("Good Morning Sir")
+// }
+// else if(gendar === "female") {
+//     document.write("Good Morning Mam")
+// }
+// else(
+//     document.write("Please enter correct value Male or Female")
+// );
+
+//qns 3
+
+// let color = prompt("Enter the color of the traffic signal (Red,Yellow,Green)")
+
+// if (color === "Red") {
+//     document.write("Must Stop");
+// }
+// else if (color === "Yellow") {
+//     document.write("Ready to Go");
+// }
+// else if (color === "Green"){
+// document.write("Go")
+// }
+// else document.write("Please enter only red,green,yellow");
+
+//qns 4
+
+// var a = 4;
+// if (++a === 5){
+//     alert(`"given condition for variable a is true" ${a}`);
+// }
+// var b = 82; 
+// if (b++ === 83){ 
+//     alert("given condition for variable b is true"); 
+// }
+// var c = 12; 
+// if (c++ === 13){ 
+//     alert("condition 1 is true"); 
+// } 
+// if (c === 13){ 
+//     alert("condition 2 is true"); 
+// } 
+// if (++c < 14){ 
+//     alert("condition 3 is true"); 
+// } 
+// if(c === 14){ 
+//     alert("condition 4 is true"); 
+// }
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ 
+//     alert(`total cost equals to ${totalCost}`); 
+// }
+// if (true){ 
+//     alert("True"); 
+// } 
+// if (false){ 
+//     alert("False");
+// }
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+//qns 5
+ 
+    // let subject1 = 90;
+    // let subject2 = 90;
+    // let subject3 = 90;
+    // let overAllmarks = 300  
+    // let totalMarks = subject1 + subject2 + subject3;
+
+    // let totalObtained = totalMarks;
+    // let percentage = totalObtained / totalMarks * 100;
+
+    //  let grade, remarks;
+    // if (percentage >= 90) {
+    //     grade = "A";
+    //     remarks = "Excellent";
+    // } else if (percentage >= 80) {
+    //     grade = "B";
+    //     remarks = "Very Good";
+    // } else if (percentage >= 70) {
+    //     grade = "C";
+    //     remarks = "Good";
+    // } else if (percentage >= 60) {
+    //     grade = "D";
+    //     remarks = "Fair";
+    // } else if (percentage >= 50) {
+    //     grade = "E";
+    //     remarks = "Pass";
+    // } else {
+    //     grade = "F";
+    //     remarks = "Fail";
+    // }
+
+    // totalMarks = prompt("Enter Subject1 Subject2 & Subject3 Number ");
+
+    // document.write(`Total Marks ${overAllmarks} \n Marks Obtained ${totalObtained} \n Percentage : ${percentage} \n grade ${grade} \n Remarks:${remarks}`)
+
+    //qns no 7
+
+// let score = prompt("guess the secret number(1-10:")
+// let grade = ""
+
+// if(score >= 0 && score <= 10) {
+//     document.write("“Bingo! Correct answer")
+// }
+// else if(score >= 11) {
+//     document.write("Close enough to the correct answer")
+// }
+
+//qns 8
+
+// var userInput = prompt("Enter a number to check if it's divisible by 3:");
+
+
+// var number = parseInt(userInput, 10);
+
+// if (!isNaN(number)) {
+//     if (number % 3 === 0) {
+//         alert(number + " is divisible by 3.");
+//     } else {
+//         alert(number + " is not divisible by 3.");
+//     }
+// } else {
+//     alert("Please enter a valid number.");
+// }
+
+//qns no 9
+
+// let num = 10
+
+// if( num % 2 === 0) {
+//     console.log(num,"is even")
+// }else(console.log(num, "is odd"))
+
+//qns no 11
+
+
+// var userInput = prompt("Enter the temperature:");
+
+
+// var temperature = parseFloat(userInput);
+
+
+// if (!isNaN(temperature)) {
+  
+//     if (temperature > 40) {
+//         alert("It is too hot outside.");
+//     } else if (temperature > 30) {
+//         alert("The Weather today is Normal.");
+//     } else if (temperature > 20) {
+//         alert("Today’s Weather is cool.");
+//     } else if (temperature > 10) {
+//         alert("OMG! Today’s weather is so Cool.");
+//     } else {
+//         alert("It's quite chilly today.");
+//     }
+// } else {
+//     alert("Please enter a valid number.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
